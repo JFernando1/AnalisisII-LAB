d2d6f85f006f583c854d03aa608b63c21cba116f
fernando <fernando@fernando>
2021-03-25 15:59:58 -0600

    bash file modified

--- automatic-tags.sh
+++ automatic-tags.sh
@@ -7,6 +7,7 @@ echo ${VERSION_BITS[0]}
 echo ${VERSION_BITS[1]}
 echo ${VERSION_BITS[2]}
 
+echo "$(git tag 1.0.0)"
 
 nversion=${VERSION_BITS[2]}
 echo ${nversion:0:1}
@@ -14,6 +15,7 @@ nversion=${nversion:0:1}
 nversion=$((nversion+1))
 newversion=${VERSION_BITS[0]}.${VERSION_BITS[1]}.$nversion
 echo "Creando un nuevo tag de la version $version a $newversion"
+
 
 #echo $(git tag $newversion)"
 #echo $(git push https://bash_script:4b515ff5f9214ecd2490ba41ea7a6ef689fbb3e6@github.com/JFernando1/AnalisisII-LAB.git --tag)"
