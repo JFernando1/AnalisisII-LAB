043930a55c03c29f47045b70ce12bfd25f1eb6f5
fernando <fernando@fernando>
2021-03-25 16:16:49 -0600

    bash file modified

--- automatic-tags.sh
+++ automatic-tags.sh
@@ -3,22 +3,22 @@ echo "$version"
 
 VERSION_BITS=(${version//./ })
 
-echo ${VERSION_BITS[0]}
-echo ${VERSION_BITS[1]}
-echo ${VERSION_BITS[2]}
-echo "$(git tag -d 1.0.0)"
-echo "$(git tag 1.0.0)"
+#echo ${VERSION_BITS[0]}
+#echo ${VERSION_BITS[1]}
+#echo ${VERSION_BITS[2]}
+#echo $(git tag -d 1.0.0)"
+#echo $(git tag 1.0.0)"
 
 nversion=${VERSION_BITS[2]}
-echo ${nversion:0:1}
+#echo ${nversion:0:1}
 nversion=${nversion:0:1}
 nversion=$((nversion+1))
 newversion=${VERSION_BITS[0]}.${VERSION_BITS[1]}.$nversion
 echo "Creando un nuevo tag de la version $version a $newversion"
 
 
-#echo $(git tag $newversion)"
-#echo $(git push https://bash_script:4b515ff5f9214ecd2490ba41ea7a6ef689fbb3e6@github.com/JFernando1/AnalisisII-LAB.git --tag)"
+echo "$(git tag $newversion)"
+echo "$(git push https://bash_script:59bc8fd5d2097498827cce551dc1677eecf3960e@github.com/JFernando1/AnalisisII-LAB.git --tag)"
  
 #git remote add origin https://bash_script:4b515ff5f9214ecd2490ba41ea7a6ef689fbb3e6@github.com/JFernando1/AnalisisII-LAB.git
 #git push https://bash_script:
